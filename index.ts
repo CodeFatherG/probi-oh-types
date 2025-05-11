5e180d4fb210959798ce6d7cf62a08ad827a8d10
CodeFatherG <gdare1@yahoo.com.au>
2025-05-11 15:12:22 +1000

    Initial Commit

---
+++ index.ts
@@ -0,0 +1,2 @@
+export * from "./src/card-details";
+export * from "./src/free-card-details";
---
+++ src/card-details.ts
@@ -0,0 +1,20 @@
+import FreeCardDetails from "./free-card-details";
+
+/**
+ * Represents the details of a card in the game.
+ * @interface
+ */
+export interface CardDetails {
+    /**
+     * The quantity of the card in the deck.
+     */
+    qty?: number;
+    /**
+     * The tags associated with the card.
+     */
+    tags?: string[];
+    /**
+     * The details of a free card.
+     */
+    free?: FreeCardDetails;
+}
